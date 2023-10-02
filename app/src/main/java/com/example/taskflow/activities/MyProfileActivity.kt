7785e11bc03b29e65d0f96c6c115fd5cbf661b31
Ivan Pogiba <pogibuskaivan@gmail.com>
2023-10-02 09:50:14 +0300

    Image chooser for profile image

--- app/src/main/java/com/example/taskflow/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/taskflow/activities/MyProfileActivity.kt
@@ -1,14 +1,32 @@
 package com.example.taskflow.activities
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.taskflow.R
 import com.example.taskflow.firebase.FirestoreClass
 import com.example.taskflow.models.User
+import java.io.IOException
+import java.lang.Exception
 
 class MyProfileActivity : BaseActivity() {
+
+    companion object {
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+    }
+
+    private var mSelectedImageFileUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
@@ -16,6 +34,69 @@ class MyProfileActivity : BaseActivity() {
         setUpActionBar()
 
         FirestoreClass().loadUserData(this)
+
+        val iv_profile_user_image = findViewById<ImageView>(R.id.iv_profile_user_image)
+
+        iv_profile_user_image.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty()
+                 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                showImageChooser()
+            }
+        } else {
+            Toast.makeText(
+                this,
+                "Oops, you just denied the permission for storage. You can also allow it from settings.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun showImageChooser() {
+        var galleryIntent = Intent(Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK &&
+            requestCode == PICK_IMAGE_REQUEST_CODE &&
+            data!!.data != null) {
+            mSelectedImageFileUri = data.data
+
+            try {
+                val iv_user_image = findViewById<ImageView>(R.id.iv_profile_user_image)
+                Glide
+                    .with(this@MyProfileActivity)
+                    .load(mSelectedImageFileUri)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_user_place_holder)
+                    .into(iv_user_image)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 
     private fun setUpActionBar() {
@@ -32,7 +113,7 @@ class MyProfileActivity : BaseActivity() {
     }
 
     fun setUserDataUnUI(user: User) {
-        val iv_user_image = findViewById<ImageView>(R.id.iv_user_image)
+        val iv_user_image = findViewById<ImageView>(R.id.iv_profile_user_image)
         Glide
             .with(this@MyProfileActivity)
             .load(user.image)
