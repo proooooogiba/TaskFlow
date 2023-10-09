e0cee09db2dbb1d5f10a74252e44c136d2a62704
Ivan Pogiba <pogibuskaivan@gmail.com>
2023-10-09 12:18:42 +0300

    Loading card details to set the title of the card

--- app/src/main/java/com/example/taskflow/activities/CardDetailsActivity.kt
+++ app/src/main/java/com/example/taskflow/activities/CardDetailsActivity.kt
@@ -5,12 +5,47 @@ import android.os.Bundle
 import com.example.taskflow.R
 import com.example.taskflow.databinding.ActivityBaseBinding
 import com.example.taskflow.databinding.ActivityCardDetailsBinding
+import com.example.taskflow.models.Board
+import com.example.taskflow.utils.Constants
 
 class CardDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCardDetailsBinding
+    private lateinit var mBoardDetails: Board
+    private var mTaskListPosition = -1
+    private var mCardPosition = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCardDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        getIntentData()
+        setUpActionBar()
+    }
+
+
+    private fun setUpActionBar() {
+        setSupportActionBar(binding.toolbarCardDetailsActivity)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = mBoardDetails
+                .taskList[mTaskListPosition]
+                .cards[mCardPosition].name
+        }
+
+        binding.toolbarCardDetailsActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun getIntentData() {
+        if (intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
+        }
+        if (intent.hasExtra(Constants.TASK_LIST_ITEM_POSITION)) {
+            mTaskListPosition = intent.getIntExtra(Constants.TASK_LIST_ITEM_POSITION, -1)
+        }
+        if (intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)) {
+            mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
+        }
     }
 }
